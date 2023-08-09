d60a7e328cde5d4db8b28cea0550e62305318207
adiwahyudi02 <adi.whyd02@gmail.com>
2023-08-09 11:22:26 +0700

    feat: add Product Type

---
+++ utils/type/product.tsx
@@ -0,0 +1,13 @@
+export interface IProduct {
+    id: number;
+    title: string;
+    description: string;
+    price: string;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+}
