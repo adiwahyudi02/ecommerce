83c32c3db3c863b616860b661c9734c78dbe35ae
adiwahyudi02 <adi.whyd02@gmail.com>
2023-08-09 10:55:54 +0700

    style: remove unused comment on Sidebar Desktop Component

--- components/Sidebar/SidebarDesktop.tsx
+++ components/Sidebar/SidebarDesktop.tsx
@@ -5,16 +5,13 @@ import SidebarItem from './SidebarItem';
 
 export default function SidebarDesktop() {
     return (
-        // <div className="w-[300px] h-full bg-white rounded-xl p-5 shadow-md">
         <Card className="w-[300px] h-full bg-white rounded-xl p-5">
             <h1 className="text-lg font-bold mt-10 text-indigo-800 ml-2">
                 Ecommerce.
             </h1>
-
             <div className="mt-20">
                 <SidebarItem />
             </div>
         </Card>
-        // </div>
     );
 }
