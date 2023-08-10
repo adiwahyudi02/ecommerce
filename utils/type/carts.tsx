33951e51a8c6cb2777c34a71c8e63732c8bec332
adiwahyudi02 <adi.whyd02@gmail.com>
2023-08-10 19:28:24 +0700

    feat: add carts type

---
+++ utils/type/carts.tsx
@@ -0,0 +1,19 @@
+export interface IProductCart {
+    id: number;
+    title: string;
+    price: number;
+    quantity: number;
+    total: number;
+    discountPercentage: number;
+    discountedPrice: number;
+}
+
+export interface ICart {
+    id: number;
+    total: number;
+    discountedTotal: number;
+    userId: number;
+    totalProducts: number;
+    totalQuantity: number;
+    products: IProductCart[];
+}
