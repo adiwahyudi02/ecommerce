659479b2ba4cafd966157ad4c3724fa1938f240b
adiwahyudi02 <adi.whyd02@gmail.com>
2023-08-10 19:29:55 +0700

    feat: add useCarts hook for get carts data from api

---
+++ hooks/api/useCarts.tsx
@@ -0,0 +1,53 @@
+import useSWR from 'swr';
+import { axiosInstance } from '@/utils/helper/axios';
+import { Key, SortOrder } from 'antd/es/table/interface';
+import { ICart } from '@/utils/type/carts';
+
+interface ICartResponse {
+    carts: ICart[];
+    total: number;
+}
+
+interface ICartParams {
+    url: string;
+    limit?: number | string;
+    page?: number | string;
+    sortDirection?: SortOrder | undefined;
+    sortField?: Key | readonly Key[] | undefined,
+}
+
+const getCarts = async ({
+    url,
+    limit,
+    page,
+    sortDirection,
+    sortField,
+}: ICartParams): Promise<ICartResponse> => {
+    try {
+        const params = {
+            ...(!!page && { _page: page }),
+            ...(!!limit && { _limit: limit }),
+            ...(!!sortDirection && {
+                _order: sortDirection === 'ascend' ? 'asc' : 'desc',
+                _sort: sortField,
+            }),
+        };
+        const { data, headers } = await axiosInstance.get(url, { params });
+
+        return {
+            carts: data,
+            total: Number(headers["x-total-count"]),
+        };
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const useCarts = (params?: Omit<ICartParams, 'url'>) => {
+    const { data, isLoading } = useSWR({ url: '/carts', ...params }, getCarts);
+
+    return {
+        data,
+        isLoading,
+    }
+}
